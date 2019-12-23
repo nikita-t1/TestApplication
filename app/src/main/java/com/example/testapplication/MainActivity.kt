3a74faf72b90bbaa6177c6888af408b772ee6a7d
nikita-t1 <nikita.t2@gmx.de>
2019-12-23 22:27:37 +0100

    Added New Layout and Logic 2 (#2) (Owner)
    * DEL
    
    * new LAyout
    
    * new listener
    
    --Owner Text--

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -2,22 +2,20 @@ package com.example.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_second.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
+        button.setOnClickListener()
     }
 
-    fun something(){
+}
 
-    }
-    fun test() {
-
-
-        val no = false
-        val yes = true
-        var yess = true
-    }
+private fun Button.setOnClickListener() {
+    Log.i("Tag", "Mesage")
 }
